a5716f030f26c81e2e30c4482e110e5f506c406c
Rohan Gore <gorerohan15@gmail.com>
2022-01-14 13:50:58 +0530

    Google Doc Clone Project Added

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,18 +1,19 @@
-import img1 from '../img/instagram-clone.JPG'
-import img2 from '../img/whatsapp-clone.jpg'
-import img3 from '../img/netflix-clone.JPG'
-import img4 from '../img/spotify-clone.JPG'
-import img5 from '../img/slack-clone.JPG'
-import img6 from '../img/discord-clone.JPG'
-import img7 from '../img/google-clone.JPG'
-import img8 from '../img/react-native-tinder-clone.JPG'
-import img9 from '../img/react-native-signal-clone.jpeg'
-import img10 from '../img/angular-investenergy.JPG'
-import img11 from '../img/angular-todo.JPG'
-import img12 from '../img/machine-learning.JPG'
-import img13 from '../img/selenium.JPG'
-import img14 from '../img/chatbot.JPG'
-import img15 from '../img/deeplearning.jfif'
+import img1 from '../img/google-docs-clone.JPG'
+import img2 from '../img/instagram-clone.JPG'
+import img3 from '../img/whatsapp-clone.jpg'
+import img4 from '../img/netflix-clone.JPG'
+import img5 from '../img/spotify-clone.JPG'
+import img6 from '../img/slack-clone.JPG'
+import img7 from '../img/discord-clone.JPG'
+import img8 from '../img/google-clone.JPG'
+import img9 from '../img/react-native-tinder-clone.JPG'
+import img10 from '../img/react-native-signal-clone.jpeg'
+import img11 from '../img/angular-investenergy.JPG'
+import img12 from '../img/angular-todo.JPG'
+import img13 from '../img/machine-learning.JPG'
+import img14 from '../img/selenium.JPG'
+import img15 from '../img/chatbot.JPG'
+import img16 from '../img/deeplearning.jfif'
 
 
 
@@ -21,129 +22,137 @@ const porfolios = [
         id: 1,
         category: 'React-Clones',
         image: img1,
-        link1:'https://github.com/rohangore1999/REACT-INSTAGRAM-CLONE',
-        link2:'https://react-instagram-clone-fo5mawpwm-rohangore1999.vercel.app/',
-        text: 'Instagram-Clone-React'
+        link1:'https://github.com/rohangore1999/REACT-GOOGLE-DOCS-CLONE',
+        link2:'https://react-google-docs-clone-hs6rohzvg-rohangore1999.vercel.app/',
+        text: 'Google-Docs-Clone-React'
     },
     {
         id: 2,
         category: 'React-Clones',
         image: img2,
+        link1:'https://github.com/rohangore1999/REACT-INSTAGRAM-CLONE',
+        link2:'https://react-instagram-clone-fo5mawpwm-rohangore1999.vercel.app/',
+        text: 'Instagram-Clone-React'
+    },
+    {
+        id: 3,
+        category: 'React-Clones',
+        image: img3,
         link1:'https://github.com/rohangore1999/REACT-WHATSAPP-CLONE',
         link2:'https://react-whatsapp-clone-lake.vercel.app/',
         text: 'Whatsapp-Clone-React'
     },
 
     {
-        id: 3,
+        id: 4,
         category: 'React-Clones',
-        image: img3,
+        image: img4,
         link1:'https://github.com/rohangore1999/React-NETFLIX-CLONE',
         link2:'http://react-netflix-clone.investenergy.in/',
         text: 'Netflix-Clone-React'
     },
 
     {
-        id: 4,
+        id: 5,
         category: 'React-Clones',
-        image: img4,
+        image: img5,
         link1:'https://github.com/rohangore1999/REACT-SPOTIFY-CLONE',
         link2:'https://spotify-clone-4a547.web.app/',
         text: 'Spotify-Clone-React'
     },
     {
-        id: 5,
+        id: 6,
         category: 'React-Clones',
-        image: img5,
+        image: img6,
         link1:'https://github.com/rohangore1999/REACT-SLACK-CLONE',
         link2:'https://slack-clone-63483.web.app/',
         text: 'Slack-Clone-React'
     },
 
     {
-        id: 6,
+        id: 7,
         category: 'React-Clones',
-        image: img6,
+        image: img7,
         link1:'https://github.com/rohangore1999/REACT-DISCORD-CLONE',
         link2:'https://discord-clone-de39b.web.app/',
         text: 'Discord-Clone-React'
     },
 
     {
-        id: 7,
+        id: 8,
         category: 'React-Clones',
-        image: img7,
+        image: img8,
         link1:'https://github.com/rohangore1999/REACT-GOOGLE-CLONE',
         link2:'https://clone-aa449.web.app/',
         text: 'Google-Clone-React'
     },
 
     {
-        id: 8,
+        id: 9,
         category: 'React-Native-Expo',
-        image: img8,
+        image: img9,
         link1:'https://github.com/rohangore1999/REACT-NATIVE-EXPO-TINDER-CLONE',
         link2: 'https://expo.dev/artifacts/eb202922-3855-4edf-9a66-011389576fd1',
         text: 'Tinder-Clone-React-Native-Expo'
     },
 
     {
-        id: 9,
+        id: 10,
         category: 'React-Native-Expo',
-        image: img9,
+        image: img10,
         link1:'https://github.com/rohangore1999/REACT-NATIVE-SIGNAL-CLONE',
         link2: 'https://expo.dev/artifacts/6c2843ad-a116-4e04-a062-752ef1c7cb5c',
         text: 'Signal-Clone-React-Native-Expo'
     },
 
     {
-        id: 10,
+        id: 11,
         category: 'Angular',
-        image: img10,
+        image: img11,
         link1:'https://github.com/rohangore1999/Solar-Web-Angular',
         link2:'https://investenergy.in/',
         text: 'Invest Energy'
     },
     {
-        id: 11,
+        id: 12,
         category: 'Angular',
-        image: img11,
+        image: img12,
         link1:'https://github.com/rohangore1999/Angular-todo',
         link2: false,
         text: 'Todo List'
     },
 
     {
-        id: 12,
+        id: 13,
         category: 'Machine Learning',
-        image: img12,
+        image: img13,
         link1:'https://github.com/rohangore1999/Machine-Learning',
         link2: false,
         text: 'ML - Projects'
     },
 
     {
-        id: 13,
+        id: 14,
         category: 'Web Scraping',
-        image: img13,
+        image: img14,
         link1:'https://github.com/rohangore1999/Selenium',
         link2: false,
         text: 'Selenium'
     },
 
     {
-        id: 14,
+        id: 15,
         category: 'Chatbot',
-        image: img14,
+        image: img15,
         link1:'https://github.com/rohangore1999/Selenium',
         link2: false,
         text: 'Rasa NLU'
     },
 
     {
-        id: 15,
+        id: 16,
         category: 'Deep Learning',
-        image: img15,
+        image: img16,
         link1:'https://github.com/rohangore1999/Selenium',
         link2: false,
         text: 'DL - Projects'
