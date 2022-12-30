eb88e1197315ee7f5329b004fd869e9f4e7f0cbe
rohan.gore <rohan.gore@cashfree.com>
2022-12-30 22:56:37 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { Route, Switch as Switching } from 'react-router';
-import styled from 'styled-components'
-import Sidebar from './Components/Sidebar';
-import HomePage from './Pages/HomePage';
-import AboutPage from './Pages/AboutPage';
-import ResumePage from './Pages/ResumePage';
-import PortfoliosPage from './Pages/PortfoliosPage';
-import BlogsPage from './Pages/BlogsPage';
-import ContactPage from './Pages/ContactPage';
-import Switch from '@material-ui/core/Switch'
-import Brightness4Icon from '@material-ui/icons/Brightness4'
-import { useEffect, useState } from 'react';
-import MenuIcon from '@material-ui/icons/Menu';
-import Hamburger from 'hamburger-react';
-import { IconButton } from '@material-ui/core';
-
+import { Route, Switch as Switching } from "react-router";
+import styled from "styled-components";
+import Sidebar from "./Components/Sidebar";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ResumePage from "./Pages/ResumePage";
+import PortfoliosPage from "./Pages/PortfoliosPage";
+import BlogsPage from "./Pages/BlogsPage";
+import ContactPage from "./Pages/ContactPage";
+import Switch from "@material-ui/core/Switch";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import { useEffect, useState } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import Hamburger from "hamburger-react";
+import { IconButton } from "@material-ui/core";
 
 function App() {
   // usestate to toggle dark/light theme; by default it is dark-theme
-  const [theme, setTheme] = useState('dark-theme');
+  const [theme, setTheme] = useState("dark-theme");
   const [checked, setChecked] = useState(false);
   const [navToggle, setNavToggle] = useState(false);
   const [HamToggle, setHamToggle] = useState(false);
@@ -25,25 +24,26 @@ function App() {
   //when the theme change useEffect will run and set documentElement's class as theme
   useEffect(() => {
     document.documentElement.className = theme;
-  }, [theme])
+  }, [theme]);
 
   const themeToggler = () => {
-    if (theme === 'light-theme') {
-      setTheme('dark-theme');
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
       setChecked(false);
-    }
-    else {
-      setTheme('light-theme');
+    } else {
+      setTheme("light-theme");
       setChecked(true);
     }
-
-  }
-
+  };
 
   return (
     <div className="App">
-
-      <Sidebar navToggle={navToggle} setNavToggle={setNavToggle} HamToggle={HamToggle} setHamToggle={setHamToggle}/>
+      <Sidebar
+        navToggle={navToggle}
+        setNavToggle={setNavToggle}
+        HamToggle={HamToggle}
+        setHamToggle={setHamToggle}
+      />
 
       <div className="theme">
         <div className="light-dark-mode">
@@ -52,23 +52,26 @@ function App() {
           </div>
 
           <div className="right-content">
-            <Switch value="" checked={checked} inputProps={{ 'aria-label': '' }} onClick={themeToggler} />
+            <Switch
+              value=""
+              checked={checked}
+              inputProps={{ "aria-label": "" }}
+              onClick={themeToggler}
+            />
           </div>
         </div>
       </div>
 
       <div className="ham-burger-menu">
-        <IconButton onClick={() => {
-          setNavToggle(!navToggle)
-          setHamToggle(!HamToggle)
-          }
-        }>
-
-          <Hamburger duration={0.8} toggled={HamToggle}
-          />
+        <IconButton
+          onClick={() => {
+            setNavToggle(!navToggle);
+            setHamToggle(!HamToggle);
+          }}
+        >
+          <Hamburger duration={0.8} toggled={HamToggle} />
         </IconButton>
       </div>
-
 
       <MainContentStyled>
         <div className="lines">
@@ -78,10 +81,7 @@ function App() {
           <div className="line-4"></div>
         </div>
 
-
-
         <Switching>
-
           <Route path="/" exact>
             <HomePage />
           </Route>
@@ -105,12 +105,8 @@ function App() {
           <Route path="/contact" exact>
             <ContactPage />
           </Route>
-
-
         </Switching>
-
       </MainContentStyled>
-
     </div>
   );
 }
@@ -122,11 +118,11 @@ const MainContentStyled = styled.main`
   margin-left: 16.3rem;
   min-height: 100vh;
 
-  @media screen and (max-width: 1200px){
+  @media screen and (max-width: 1200px) {
     margin-left: 0;
   }
 
-  .lines{
+  .lines {
     position: absolute;
     min-height: 100%;
     width: 100%;
@@ -135,10 +131,13 @@ const MainContentStyled = styled.main`
     opacity: 0.4;
     z-index: -1;
 
-    .line-1, .line-2, .line-3, .line-4 {
+    .line-1,
+    .line-2,
+    .line-3,
+    .line-4 {
       width: 1px;
       min-height: 100vh;
-      background-color: var(--border-color)
+      background-color: var(--border-color);
     }
   }
-`
+`;
